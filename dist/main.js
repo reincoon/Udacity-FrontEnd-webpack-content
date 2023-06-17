05c9d2b9f4661ad9c5b0d3f5d0f4f6874ccb3dcc
reincoon <irinawalch@gmail.com>
2023-06-18 00:21:41 +0100

    last change

---
+++ dist/main.js
@@ -0,0 +1,15 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**************************************!*\
+  !*** ./src/client/js/nameChecker.js ***!
+  \**************************************/
+function checkForName(inputText) {
+  console.log("::: Running checkForName :::", inputText);
+  var names = ["Picard", "Janeway", "Kirk", "Archer", "Georgiou"];
+  if (names.includes(inputText)) {
+    alert("Welcome, Captain!");
+  }
+}
+/******/ })()
+;
+//# sourceMappingURL=main.js.map
